2dc9ce281d745f6d821814fbede400f13ca4a7c9
Paulo Anjos <pnmanjos@hotmail.com>
2018-09-11 14:29:39 -0300

    Faltando HttpOverrides

--- API/PromotionApi/Startup.cs
+++ API/PromotionApi/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
