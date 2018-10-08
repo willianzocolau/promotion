ef6b2199b28b14ddfee26690ecd4d47948af7de3
Maegima <maegima.andre@gmail.com>
2018-10-08 14:37:25 -0300

    backend: home cards

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -15,15 +15,14 @@ import { HTTP } from '@ionic-native/http';
 })
 
 export class HomePage {
-
-
+  public promotions = [];
   constructor(private storage: Storage,
               public nav: NavController,
               public popoverCtrl: PopoverController,
               private user: UserData,
               private server: ServerStrings,
               private http: HTTP) {
-    
+            
     /*let url: string = this.server.user();
     let headers = {
       'Authorization': 'Bearer ' + this.user.getToken()
@@ -46,6 +45,25 @@ export class HomePage {
     .catch(error => {
       console.log(error);
     });*/
+    this.promotions = [
+        {
+            "username": "andre",
+            "date": "10-09-2018",
+            "description": "Mouse muito legal",
+            "likes": 12,
+            "comments": 7,
+            "image_url": "https://purepng.com/public/uploads/large/purepng.com-pc-mousepc-mousepcmouselaptop-mouse-1701528347542ws1aa.png"
+        },
+        {
+            "username": "andre",
+            "date": "11-08-2018",
+            "description": "outro Mouse muito legal",
+            "likes": 16,
+            "comments": 10,
+            "image_url": "https://purepng.com/public/uploads/large/purepng.com-pc-mousepc-mousepcmouselaptop-mouse-1701528347542ws1aa.png"
+        },
+
+    ];
   }
 
   // to go account page
