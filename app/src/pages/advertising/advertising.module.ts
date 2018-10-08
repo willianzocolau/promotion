b304e30ae55188aaddb8a50fb6b0cfea5d8a5567
Taaaaaar <tarcisiogabriel.philips@gmail.com>
2018-10-08 08:10:17 -0300

    Criada a estrutura básica da página de anúncios

---
+++ app/src/pages/advertising/advertising.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AdvertisingPage } from './advertising';
+
+@NgModule({
+  declarations: [
+    AdvertisingPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AdvertisingPage),
+  ],
+})
+export class AdvertisingPageModule {}
