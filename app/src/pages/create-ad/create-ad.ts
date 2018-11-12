1ee010ac4fe9847c4a67b6a74428373fe7c1c2b6
Washington <washington_sp2007@hotmail.com>
2018-11-12 10:07:44 -0200

    Formulário da page create-ad
    Foi consertado o erro do botão de criar anúncios e foi feito todo o formulário de preenchimento, faltando apenas a ligação com o Banco

--- app/src/pages/create-ad/create-ad.ts
+++ app/src/pages/create-ad/create-ad.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the CreateAdPage page.
@@ -15,7 +16,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreateAdPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public form : FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public formBuilder: FormBuilder) {
+      this.form = this.formBuilder.group({
+        name: ['', Validators.required],
+        price: ['',Validators.required],
+        image: ['',Validators.required],
+        store_id: ['',Validators.required],
+        state_id: ['',Validators.required]
+      });
+  }
+
+  logForm(){
+    console.log(this.form.value)
   }
 
   ionViewDidLoad() {
