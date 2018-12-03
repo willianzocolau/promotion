0106176296e17456ccc37b0f51544ca841a508dd
Maegima <maegima.andre@gmail.com>
2018-12-03 14:19:06 -0200

    app: ad page listagem de comentarios

--- app/src/pages/advertising/advertising.ts
+++ app/src/pages/advertising/advertising.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { LoadingController, AlertController, NavController, NavParams, ViewController } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
+
+import { UserData } from '../../providers/userData';
+import { ServerStrings } from '../../providers/serverStrings';
 
 @Component({
   selector: 'page-advertising',
@@ -7,13 +11,48 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
 })
 export class AdvertisingPage {
   public item: any = undefined;
+  public votes = [];
   constructor(public navCtrl: NavController, 
     public navParams: NavParams, 
-    public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    public http: HTTP,
+    public user: UserData,
+    public server: ServerStrings,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController) {
       this.item = this.navParams.data;
       console.log(this.item);
+      this.dadosPromocao(this.item.promotion.id);
   }
   closeModal(){
     this.viewCtrl.dismiss(this.item);
+  }
+
+  dadosPromocao(id: number){
+    let loading = this.loadingCtrl.create({ content: 'Carregando...' });
+    loading.present();
+
+    let endpoint = this.server.promotionId(id);
+    
+    let headers = {
+      'Authorization': 'Bearer ' + this.user.getToken(),
+      'Content-type': 'application/json'
+    };
+    
+    this.http.get(endpoint, {}, headers)
+    .then(response => {
+      let promotion = JSON.parse(response.data);
+      console.log(promotion);
+      this.votes = promotion.votes;
+      loading.dismiss();
+    })
+    .catch(exception =>{
+      let dados = JSON.parse(exception.error);
+      let msg = this.alertCtrl.create({
+        message: "Erro: " + dados.error
+      });
+      loading.dismiss();
+      msg.present();
+    });
   }
 }
